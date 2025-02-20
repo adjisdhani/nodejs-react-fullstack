f57e3899446d4ca9a5d50d32a4bbfd75936a207f
adjis.ramadhani <adjis.ramadhani@koltiva.com>
2025-02-20 12:17:28 +0700

    all (2)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Books from './Books';
+import CreateBook from './CreateBook';
+import UpdateBook from './UpdateBook';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Books />}> </Route>
+              <Route path='/create' element={<CreateBook />}> </Route>
+              <Route path='/update/:id' element={<UpdateBook />}> </Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
